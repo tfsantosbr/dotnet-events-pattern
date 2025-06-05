691ee2a207b92ea89300ca36c354f8cc0512ecfe
Tiago Santos <tfsantosbr@gmail.com>
2025-06-05 17:45:20 -0300

    update do net9

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dotnet-event-patterns
+
+## Run Application
+
+```bash
+dotnet publish src/EventPatterns.Example -c Release
+dotnet src/EventPatterns.Example/bin/Release/net9.0/publish/EventPatterns.Example.dll
+```
--- src/EventPatterns.Example/Benchmarking/EventPatternsBenchmark.cs
+++ src/EventPatterns.Example/Benchmarking/EventPatternsBenchmark.cs
@@ -1,4 +1,6 @@
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
 using EventPatterns.Example.Abstractions;
 using EventPatterns.Example.Events;
 using EventPatterns.Example.Extensions;
@@ -10,6 +12,11 @@ using Microsoft.Extensions.DependencyInjection;
 namespace EventPatterns.Example.Benchmarking;
 
 [MemoryDiagnoser(true)]
+[SimpleJob(RuntimeMoniker.Net90)]
+[GcServer(true)]
+[GcConcurrent(true)]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
 public class EventPatternsBenchmark
 {
     private IEventDispatcher _eventDispatcher = null!;
@@ -46,23 +53,66 @@ public class EventPatternsBenchmark
         _publisher = serviceProvider.GetRequiredService<IPublisher>();
     }
 
-    [Benchmark]
+    [Params(100, 50, 1000)]
+    public int EventCount { get; set; }
+
+    [Benchmark(Baseline = true)]
     public async Task DomainEventsWithoutMediatr()
     {
-        var userCreatedEvent = new UserCreatedEvent(Guid.NewGuid());
-        await _eventDispatcher.DispatchAsync(userCreatedEvent, CancellationToken.None);
+        for (int i = 0; i < EventCount; i++)
+        {
+            var userCreatedEvent = new UserCreatedEvent(Guid.NewGuid());
+            await _eventDispatcher.DispatchAsync(userCreatedEvent, CancellationToken.None);
 
-        var orderPlacedEvent = new OrderPlacedEvent(Guid.NewGuid());
-        await _eventDispatcher.DispatchAsync(orderPlacedEvent, CancellationToken.None);
+            var orderPlacedEvent = new OrderPlacedEvent(Guid.NewGuid());
+            await _eventDispatcher.DispatchAsync(orderPlacedEvent, CancellationToken.None);
+        }
     }
 
     [Benchmark]
-    public async Task DomainEventsWithtMediatr()
+    public async Task DomainEventsWithMediatr()
     {
-        var userCreatedEvent = new UserCreatedEvent(Guid.NewGuid());
-        await _publisher.Publish(userCreatedEvent);
+        for (int i = 0; i < EventCount; i++)
+        {
+            var userCreatedEvent = new UserCreatedEvent(Guid.NewGuid());
+            await _publisher.Publish(userCreatedEvent);
 
-        var orderPlacedEvent = new OrderPlacedEvent(Guid.NewGuid());
-        await _publisher.Publish(orderPlacedEvent);
+            var orderPlacedEvent = new OrderPlacedEvent(Guid.NewGuid());
+            await _publisher.Publish(orderPlacedEvent);
+        }
+    }
+
+    [Benchmark]
+    public async Task BatchEventsWithoutMediatr()
+    {
+        var events = new List<DomainEvent>();
+
+        for (int i = 0; i < EventCount; i++)
+        {
+            events.Add(new UserCreatedEvent(Guid.NewGuid()));
+            events.Add(new OrderPlacedEvent(Guid.NewGuid()));
+        }
+
+        foreach (var @event in events)
+        {
+            await _eventDispatcher.DispatchAsync(@event, CancellationToken.None);
+        }
+    }
+
+    [Benchmark]
+    public async Task BatchEventsWithMediatr()
+    {
+        var events = new List<DomainEvent>();
+
+        for (int i = 0; i < EventCount; i++)
+        {
+            events.Add(new UserCreatedEvent(Guid.NewGuid()));
+            events.Add(new OrderPlacedEvent(Guid.NewGuid()));
+        }
+
+        foreach (var @event in events)
+        {
+            await _publisher.Publish(@event);
+        }
     }
 }
